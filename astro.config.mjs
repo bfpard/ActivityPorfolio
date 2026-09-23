0cdcee0a669119aec51336c910cf020cb16d6b71
FelcorDev <92054081+bfpard@users.noreply.github.com>
2026-09-23 15:00:25 -0500

    BF: Add CD

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bfpard.github.io',
+  base: '/ActivityPorfolio',
   devToolbar: {
     enabled: false,
   },
